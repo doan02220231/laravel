6190b979c1956df540c35d5e9d7555ed68a21cfe
doan02220231 <maivandoan2k2@gmail.com>
2023-12-24 21:37:47 +0700

    project laravel

---
+++ shopbanhanglaravel/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('admin_layout')
+@section('admin_content')
+<h3><center>Chào mừng đến với trang Admin : Mai Văn Đoàn- 20CT1 - 2051220033</center></h3>
+@endsection
---
+++ shopbanhanglaravel/resources/views/pages/checkout/handcash.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('content')
+
+<section id="cart_items">
+		<div class="container">
+			
+			<div class="review-payment">
+				<h2>Đã đặt hàng thành công.Xin Cảm Ơn!!</h2>
+			</div>
+			
+			
+			
+		</div>
+	</section> <!--/#cart_items-->
+
+@endsection
